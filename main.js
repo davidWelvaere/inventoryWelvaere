b4aaf0977aefc8a2297cfffd8f24050313bc5258
david <david@welvaere.nl>
2024-09-12 13:31:32 +0200

    init commit

---
+++ main.js
@@ -0,0 +1,8 @@
+document.getElementById('save').addEventListener('click', (event) => {
+    event.preventDefault();
+
+    const reference = document.getElementById('reference').value;
+    const quantity = document.getElementById('quantity').value;
+
+});
+
